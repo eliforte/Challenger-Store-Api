943e26d0bed85e575439cb62a9b394db68700d26
Elias Forte <eliasforte.dev@gmail.com>
2022-05-04 21:08:26 -0300

    [feat]: add all rotes into file root for export

---
+++ src/controllers/root.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routerLogin = require('./users/login/route');
+const routerRegister = require('./users/register/route');
+const { Login, Register } = require('../global/middlewares/validation');
+
+const root = express.Router({ mergeParams: true });
+
+root.use('/login', Login, routerLogin);
+root.use('/register', Register, routerRegister);
+
+module.exports = root;
