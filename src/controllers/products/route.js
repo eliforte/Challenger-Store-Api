f86fa29befca705e1fa7e4984fc4667ccae98d71
Elias Forte <eliasforte.dev@gmail.com>
2022-05-05 04:59:09 -0300

    [feat]: middleware VerifyIsAdmin into route prod

--- src/controllers/products/route.js
+++ src/controllers/products/route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const Controllers = require('.');
 const Validation = require('../../middlewares/validation');
+const Authorization = require('../../middlewares/auth');
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', Controllers.GetAll);
-router.post('/', Validation.CreateProduct, Controllers.Create);
-router.put('/:id',Validation.EditProduct, Controllers.Edit);
-router.delete('/:id', Controllers.Delete);
+router.post('/', Authorization.VerifyIsAdmin , Validation.CreateProduct, Controllers.Create);
+router.put('/:id',Authorization.VerifyIsAdmin, Validation.EditProduct, Controllers.Edit);
+router.delete('/:id', Authorization.VerifyIsAdmin, Controllers.Delete);
 
 module.exports = router;
