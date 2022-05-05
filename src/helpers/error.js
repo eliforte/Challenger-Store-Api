faf4222a2e3dd37a3e98db86c8be618fb4cc8f00
Elias Forte <eliasforte.dev@gmail.com>
2022-05-04 21:09:22 -0300

    [feat]: helpers error

---
+++ src/helpers/error.js
@@ -0,0 +1,14 @@
+class ApiError extends Error {
+  constructor(message, status) {
+    super(message, status);
+    this.message = message;
+    this.status = status;
+  }
+
+  static NewError(err) {
+    const { status, message } = err;
+    throw new ApiError(message, status);
+  }
+}
+
+module.exports = { ApiError };
