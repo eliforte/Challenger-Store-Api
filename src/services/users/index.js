7f8d14f25f771f9093d455082c492bbd4bfa9ff3
Elias Forte <eliasforte.dev@gmail.com>
2022-05-05 02:03:21 -0300

    [fix]: changing password encryption

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const bcrypt = require('bcrypt');
-const salt = process.env.SALT || 15;
 const { FindByEmail, Create, FindById } = require('../../database/users');
 const { ApiError: { NewError } } = require('../../helpers/error');
 const { CreateToken } = require('../../middlewares/auth');
@@ -9,11 +8,16 @@ const { EMAIL_EXIST_409, USER_NOT_EXIST_404, INCORRECT_401 } = require('../../he
 module.exports.CreateUser = async (email, password, name, role, balance) => {
   const userExist = await FindByEmail(email);
   if (userExist) return NewError(EMAIL_EXIST_409);
-  const cryptoPassword = bcrypt.hash(salt, password);
+
+  const cryptoPassword = await bcrypt.hash(password, 10);
+
   const newUser = await Create({ email, password: cryptoPassword, name, role, balance });
   const findNewUser = await  FindById(newUser.insertedId);
+
   delete findNewUser.password;
+  newUser.role = role
   const token = CreateToken(newUser);
+
   return {
     user: { ...findNewUser },
     token,
@@ -23,12 +27,13 @@ module.exports.CreateUser = async (email, password, name, role, balance) => {
 module.exports.LoginUser = async ({ email, password }) => {
   const userExist = await FindByEmail(email);
   if (!userExist) return NewError(USER_NOT_EXIST_404);
-  const cryptoPassword = bcrypt.compare(password, userExist.password);
-  if (cryptoPassword) {
-    return NewError(INCORRECT_401);
-  }
+
+  const cryptoPassword = await bcrypt.compare(password, userExist.password);
+  if (cryptoPassword) return NewError(INCORRECT_401);
+  
   delete userExist.password;
   const token = CreateToken(userExist);
+  
   return {
     user: { ...userExist },
     token,
