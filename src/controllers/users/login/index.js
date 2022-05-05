18b521d4247a80be81d3a6250154f3183db602d4
Elias Forte <eliasforte.dev@gmail.com>
2022-05-04 23:07:36 -0300

    [fix]: change syntax

--- src/controllers/users/login/index.js
+++ src/controllers/users/login/index.js
@@ -4,7 +4,7 @@ const { LoginUser } = require('../../../services/users');
 module.exports.Login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const loginAccepted = await LoginUser({ email, password });
+    const loginAccepted = await LoginUser(email, password);
     return res.status(ACCEPTED).json(loginAccepted);
   } catch (err) {
     next(err)
--- src/controllers/users/register/index.js
+++ src/controllers/users/register/index.js
@@ -3,8 +3,8 @@ const { CreateUser } = require('../../../services/users');
 
 module.exports.Register = async (req, res, next) => {
   try {
-    const { email, password, name } = req.body;
-    const newUser = await CreateUser({ email, password, name });
+    const { email, password, name, role, balance } = req.body;
+    const newUser = await CreateUser(email, password, name, role, balance);
     return res.status(CREATED).json(newUser);
   } catch (err) {
     next(err);
