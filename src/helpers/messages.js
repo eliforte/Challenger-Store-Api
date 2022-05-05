123db6b4fdb1d5df25b78ae67d2901bcc537af1e
Elias Forte <eliasforte.dev@gmail.com>
2022-05-05 18:38:50 -0300

    [feat]: new error messages

--- src/helpers/messages.js
+++ src/helpers/messages.js
@@ -9,3 +9,9 @@ module.exports.INCORRECT_401 = { message: 'Incorrect username or password', stat
 module.exports.USER_NOT_EXIST_404 = { message: 'User not exist', status: NOT_FOUND };
 module.exports.INVALID_ENTRIES_400 = { message: 'Invalid entries', status: BAD_REQUEST };
 module.exports.NOT_ADMIN_401 = { message: 'Restricted to administrators', status: UNAUTHORIZED };
+module.exports.INVALID_ID_400 = { message: 'Invalid product ID', status: BAD_REQUEST };
+module.exports.PRODUCT_NOT_EXIST_404 = { message: 'Product not exist', status: NOT_FOUND };
+module.exports.PRODUCT_EXIST_409 = { message: 'Product already registered', status: CONFLICT };
+module.exports.BALANCE_NOT_ENOUGH_409 = { message: 'Balance not enough', status: CONFLICT };
+module.exports.QUANTITY_NOT_ENOUGH_409 = { message: 'Quantity not enough', status: CONFLICT };
+
