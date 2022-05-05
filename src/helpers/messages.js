8f00f3c1f5cfb81830f37d1d9e775ed6173f847f
Elias Forte <eliasforte.dev@gmail.com>
2022-05-04 21:13:45 -0300

    [feat]: helpers message error

---
+++ src/helpers/messages.js
@@ -0,0 +1,10 @@
+const {
+  UNAUTHORIZED, CONFLICT, NOT_FOUND, BAD_REQUEST,
+} = require('http-status-codes').StatusCodes;
+
+module.exports.JWT_MALFORMED_401 = { message: 'Jwt malformed', status: UNAUTHORIZED };
+module.exports.MISSING_TOKEN_401 = { message: 'Missing auth token', status: UNAUTHORIZED };
+module.exports.EMAIL_EXIST_409 = { message: 'Email already registered', status: CONFLICT };
+module.exports.INCORRECT_401 = { message: 'Incorrect username or password', status: UNAUTHORIZED };
+module.exports.USER_NOT_EXIST_404 = { message: 'User not exist', status: NOT_FOUND };
+module.exports.INVALID_ENTRIES_400 = { message: 'Invalid entries', status: BAD_REQUEST };
