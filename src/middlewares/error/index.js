929d9f2cecf3c2245ec3ff8005c5a79c188138e5
Elias Forte <eliasforte.dev@gmail.com>
2022-05-06 01:01:00 -0300

    [fix]: add comment

--- src/middlewares/error/index.js
+++ src/middlewares/error/index.js
@@ -2,8 +2,9 @@ const { INTERNAL_SERVER_ERROR } = require("http-status-codes").StatusCodes;
 
 module.exports = (err, _req, res, _next) => {
   if (err && err.status) {
+    // uncomment the next line to see the error in the console
+    // console.log(err);
     return res.status(err.status).json({ message: err.message });
   }
-  console.log(err);
   return res.status(INTERNAL_SERVER_ERROR).end();
 };
