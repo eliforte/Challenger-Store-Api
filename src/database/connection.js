d6bfbdc44a7c6a5597e4d65023d9be08ea7dac26
Elias Forte <eliasforte.dev@gmail.com>
2022-05-07 20:03:32 -0300

    [feat]: docker compose

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,13 +6,16 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const client = new MongoClient(process.env.MONGODB_URL, OPTIONS);
+const url = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL : 'mongodb://localhost:27017/voll-solutions-chanllenger';
+
+const client = new MongoClient(url, OPTIONS);
 
 const connection = async () => {
   try {
     client.connect();
     client.on('error', (error) => console.error(error));
     client.once('open', () => console.log('Connected to the database!'));
+    console.log(url);
   } catch (err) {
     console.log('Connection failed');
   }
